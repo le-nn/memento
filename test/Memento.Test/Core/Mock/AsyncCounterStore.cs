79590488c3eefea649d1efeb8e25ccb2a6f5088d
le-nn <renpei.okada@outlook.com>
2023-02-17 20:11:40 +0900

    Add subscribe command test.

---
+++ test/Memento.Test/Core/Mock/AsyncCounterStore.cs
@@ -0,0 +1,82 @@
+﻿using Memento.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento.Test.Core.Mock;
+
+using static Memento.Test.Core.Mock.AsyncCounterCommands;
+
+// Define state to manage in store
+public record AsyncCounterState {
+    public int Count { get; init; } = 0;
+    public ImmutableArray<int> History { get; init; } = ImmutableArray.Create<int>();
+    public bool IsLoading { get; init; } = false;
+}
+
+// Define command to change state and observe state change event in detail.
+public record AsyncCounterCommands : Command {
+    public record Increment : AsyncCounterCommands;
+    public record BeginLoading : AsyncCounterCommands;
+    public record EndLoading : AsyncCounterCommands;
+    public record ModifyCount(int Value) : AsyncCounterCommands;
+}
+
+public class AsyncCounterStore : Store<AsyncCounterState, AsyncCounterCommands> {
+    readonly Random _random = new(1234);
+
+    public AsyncCounterStore() : base(() => new(), Reducer) {
+    }
+
+    // State can change via Reducer and easy to observe state from command
+    // Reducer generate new state from command and current state
+    static AsyncCounterState Reducer(AsyncCounterState state, AsyncCounterCommands command) {
+        return command switch {
+            BeginLoading => state with {
+                IsLoading = true
+            },
+            EndLoading => state with {
+                IsLoading = false
+            },
+            Increment => HandleIncrement(state),
+            ModifyCount payload => state with {
+                Count = payload.Value,
+                History = state.History.Add(payload.Value),
+            },
+            _ => throw new CommandNotHandledException(command),
+        };
+
+        static AsyncCounterState HandleIncrement(AsyncCounterState state) {
+            var count = state.Count + 1;
+            return state with {
+                Count = count,
+                History = state.History.Add(count),
+            };
+        }
+    }
+
+    // "Dispatch" method can called outside of store via action (public method)
+    // Action can be async method.
+    public async Task CountUpAsync() {
+        Dispatch(new BeginLoading());
+
+        await Task.Delay(_random.Next(1, 50));
+
+        Dispatch(new Increment());
+        Dispatch(new EndLoading());
+    }
+
+    public void SetCount(int num) {
+        Dispatch(new ModifyCount(num));
+    }
+
+    public async Task SetCountWithRandomTimerAsync(int num) {
+        Dispatch(new BeginLoading());
+        await Task.Delay(_random.Next(1, 50));
+        Dispatch(new ModifyCount(num));
+        Dispatch(new EndLoading());
+    }
+}
--- test/Memento.Test/Core/StoreTest.cs
+++ test/Memento.Test/Core/StoreTest.cs
@@ -1,12 +1,11 @@
 ﻿using Memento.Core;
+using Memento.Test.Core.Mock;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static Memento.Test.Core.AsyncCounterCommands;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace Memento.Test.Core;
 
@@ -58,75 +57,33 @@ public class StoreTest {
             Assert.Equal(list, store.State.History);
         }
     }
-}
 
-// Define state to manage in store
-public record AsyncCounterState {
-    public int Count { get; init; } = 0;
-    public ImmutableArray<int> History { get; init; } = ImmutableArray.Create<int>();
-    public bool IsLoading { get; init; } = false;
-}
+    [Fact]
+    public async Task Command_CouldBeSubscribeCorrectly() {
+        var store = new AsyncCounterStore();
 
-// Define command to change state and observe state change event in detail.
-public record AsyncCounterCommands : Command {
-    public record Increment : AsyncCounterCommands;
-    public record BeginLoading : AsyncCounterCommands;
-    public record EndLoading : AsyncCounterCommands;
-    public record ModifyCount(int Value) : AsyncCounterCommands;
-}
+        var commands = new List<Command>();
+        var lastState = store.State;
+        using var subscription = store.Subscribe(e => {
+            Assert.Equal(e.Sender, store);
+            Assert.NotEqual(e.State, lastState);
+            Assert.Equal(e.LastState, lastState);
+            lastState = e.State;
+            commands.Add(e.Command);
+        });
 
-public class AsyncCounterStore : Store<AsyncCounterState, AsyncCounterCommands> {
-    readonly Random _random = new(1234);
+        await store.CountUpAsync();
+        store.SetCount(1234);
+        await store.CountUpAsync();
 
-    public AsyncCounterStore() : base(() => new(), Reducer) {
-    }
-
-    // State can change via Reducer and easy to observe state from command
-    // Reducer generate new state from command and current state
-    static AsyncCounterState Reducer(AsyncCounterState state, AsyncCounterCommands command) {
-        return command switch {
-            BeginLoading => state with {
-                IsLoading = true
-            },
-            EndLoading => state with {
-                IsLoading = false
-            },
-            Increment => HandleIncrement(state),
-            ModifyCount payload => state with {
-                Count = payload.Value,
-                History = state.History.Add(payload.Value),
-            },
-            _ => throw new CommandNotHandledException(command),
-        };
-
-        static AsyncCounterState HandleIncrement(AsyncCounterState state) {
-            var count = state.Count + 1;
-            return state with {
-                Count = count,
-                History = state.History.Add(count),
-            };
-        }
-    }
-
-    // "Dispatch" method can called outside of store via action (public method)
-    // Action can be async method.
-    public async Task CountUpAsync() {
-        Dispatch(new BeginLoading());
-
-        await Task.Delay(_random.Next(1, 50));
-
-        Dispatch(new Increment());
-        Dispatch(new EndLoading());
-    }
-
-    public void SetCount(int num) {
-        Dispatch(new ModifyCount(num));
-    }
-
-    public async Task SetCountWithRandomTimerAsync(int num) {
-        Dispatch(new BeginLoading());
-        await Task.Delay(_random.Next(1, 50));
-        Dispatch(new ModifyCount(num));
-        Dispatch(new EndLoading());
+        Assert.True(commands is [
+            AsyncCounterCommands.BeginLoading,
+            AsyncCounterCommands.Increment,
+            AsyncCounterCommands.EndLoading,
+            AsyncCounterCommands.ModifyCount(1234),
+            AsyncCounterCommands.BeginLoading,
+            AsyncCounterCommands.Increment,
+            AsyncCounterCommands.EndLoading
+        ]);
     }
 }
