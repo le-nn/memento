ca64b98f9da92ade521be9c13baf1ab687165c01
le-nn <renpei.okada@outlook.com>
2023-03-31 17:17:32 +0900

    Add supports to select Scoped or Singletone.

--- src/Memento.Blazor/StoreConfigExtension.cs
+++ src/Memento.Blazor/StoreConfigExtension.cs
@@ -4,41 +4,91 @@ using System.Reflection;
 
 namespace Memento.Blazor;
 
+/// <summary>
+/// Extension methods for configuring store-related services.
+/// </summary>
 public static class StoreConfigExtension {
-    public static IServiceCollection AddMemento(this IServiceCollection services) {
-        services.AddScoped<StoreProvider>();
+    /// <summary>
+    /// Adds the Memento service to the IServiceCollection.
+    /// </summary>
+    public static IServiceCollection AddMemento(this IServiceCollection services, bool isScoped = false) {
+        if (isScoped) {
+            services.AddScoped<StoreProvider>();
+        }
+        else {
+            services.AddSingleton<StoreProvider>();
+        }
         return services;
     }
 
-    public static IServiceCollection AddStore<TStore>(this IServiceCollection collection)
+    /// <summary>
+    /// Adds a custom store to the IServiceCollection.
+    /// </summary>
+    public static IServiceCollection AddStore<TStore>(this IServiceCollection collection, bool isScoped = false)
         where TStore : class, IStore {
-        collection.AddScoped<TStore>()
-            .AddScoped<IStore>(p => p.GetRequiredService<TStore>());
+        if (isScoped) {
+            collection.AddScoped<TStore>()
+                .AddScoped<IStore>(p => p.GetRequiredService<TStore>());
+        }
+        else {
+            collection.AddSingleton<TStore>()
+                .AddSingleton<IStore>(p => p.GetRequiredService<TStore>());
+        }
         return collection;
     }
 
-    public static void ScanAssemblyAndAddStores(this IServiceCollection services, Assembly assembly) {
+    /// <summary>
+    /// Scans the assembly and adds all stores that implements <see cref="IStore"/> to the IServiceCollection.
+    /// </summary>
+    public static void ScanAssemblyAndAddStores(this IServiceCollection services, Assembly assembly, bool isScoped = false) {
         foreach (var type in assembly.GetTypes().Where(t => t.IsAssignableTo(typeof(IStore)))) {
-            services.AddScoped(type)
-                .AddScoped(p => (IStore)p.GetRequiredService(type));
+            if (isScoped) {
+                services.AddScoped(type)
+                    .AddScoped(p => (IStore)p.GetRequiredService(type));
+            }
+            else {
+                services.AddSingleton(type)
+                    .AddSingleton(p => (IStore)p.GetRequiredService(type));
+            }
         }
     }
 
+    /// <summary>
+    /// Adds a custom middleware to the IServiceCollection.
+    /// </summary>
     public static IServiceCollection AddMiddleware<TMiddleware>(
         this IServiceCollection collection,
-        Func<TMiddleware> middlewareSelector
+        Func<TMiddleware> middlewareSelector,
+              bool isScoped = false
     ) where TMiddleware : Middleware {
-        collection.AddScoped<Middleware>(p => middlewareSelector());
+        if (isScoped) {
+            collection.AddScoped<Middleware>(p => middlewareSelector());
+        }
+        else {
+            collection.AddSingleton<Middleware>(p => middlewareSelector());
+        }
         return collection;
     }
 
-    public static IServiceCollection AddMiddleware<TMiddleware>(this IServiceCollection collection)
+    /// <summary>
+    /// Adds a custom middleware to the IServiceCollection.
+    /// </summary>
+    public static IServiceCollection AddMiddleware<TMiddleware>(this IServiceCollection collection, bool isScoped = false)
         where TMiddleware : Middleware {
-        collection.AddScoped<TMiddleware>();
-        collection.AddScoped<Middleware, TMiddleware>(p => p.GetRequiredService<TMiddleware>());
+        if (isScoped) {
+            collection.AddScoped<TMiddleware>();
+            collection.AddScoped<Middleware, TMiddleware>(p => p.GetRequiredService<TMiddleware>());
+        }
+        else {
+            collection.AddSingleton<TMiddleware>();
+            collection.AddSingleton<Middleware, TMiddleware>(p => p.GetRequiredService<TMiddleware>());
+        }
         return collection;
     }
 
+    /// <summary>
+    /// Gets the StoreProvider from the IServiceProvider.
+    /// </summary>
     public static StoreProvider GetStoreProvider(this IServiceProvider provider) {
         return provider.GetRequiredService<StoreProvider>();
     }
